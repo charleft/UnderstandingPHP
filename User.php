124f833c75179b2eed2e2ab7e48e4c2208861d03
Carlos Rivadeneira <carlosrivadeneiraz@gmail.com>
2015-07-10 04:36:01 -0500

    Introducing  keyword

--- User.php
+++ User.php
@@ -3,18 +3,12 @@
     public $firstName;
     public $lastName;
     public function hello(){
-      return "<br>Hello";
+      return "<br>Hello $this->firstName";
     }
   }
 
   $user1 = new User();
-  $user1->firstName = "John";
-  $user1->lastName = "Doe";
+  $user1->firstName = "Jonny";
+  $user1->lastName = "Roe";
 
-  echo $user1->hello()." ".$user1->firstName." ".$user1->lastName;
-
-  $user2 = new User();
-  $user2->firstName = "Jane";
-  $user2->lastName = "Doe";
-
-  echo $user2->hello()." ".$user2->firstName." ".$user2->lastName;
+  echo $user1->hello();

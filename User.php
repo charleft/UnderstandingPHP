cbd26ff1db1681987e177a1b7c0ef2b063b7d88b
Carlos Rivadeneira <carlosrivadeneiraz@gmail.com>
2015-07-10 04:56:41 -0500

    Returning  for channing methods

--- User.php
+++ User.php
@@ -5,10 +5,18 @@
     public function hello(){
       return "<br>Hello $this->firstName";
     }
+    public function register(){
+      $message = "<br>$this->firstName registered";
+      return $this;
+    }
+    public function mail(){
+      $message = "<br>$this->firstName mailed";
+      return $this;
+    }
   }
 
   $user1 = new User();
-  $user1->firstName = "Jonny";
+  $user1->firstName = "Jane";
   $user1->lastName = "Roe";
 
-  echo $user1->hello();
+  echo $user1->register()->mail()->hello();
